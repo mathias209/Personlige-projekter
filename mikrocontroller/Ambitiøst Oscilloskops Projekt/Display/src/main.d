274c4306c3ca8f7b3e12b90dc792cc03cbd04f36
mathias209 <s255552@dtu.dk>
2026-07-08 19:04:37 +0000

    remove depend files (.d)

--- mikrocontroller/Ambitiøst Oscilloskops Projekt/Display/src/main.d
+++
@@ -1,5 +0,0 @@
-src/main.o: src/main.c include/glad/glad.h include/KHR/khrplatform.h \
- src/render.h
-include/glad/glad.h:
-include/KHR/khrplatform.h:
-src/render.h:
--- mikrocontroller/Ambitiøst Oscilloskops Projekt/Display/src/render.d
+++
@@ -1,7 +0,0 @@
-src/render.o: src/render.c include/glad/glad.h include/KHR/khrplatform.h \
- src/constants.h src/shader.vert src/shader.frag
-include/glad/glad.h:
-include/KHR/khrplatform.h:
-src/constants.h:
-src/shader.vert:
-src/shader.frag:
